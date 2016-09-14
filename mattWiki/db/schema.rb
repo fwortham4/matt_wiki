2c77004995373c3ff4a20ab71da793fae6de780c
Forrest W & Stephen Fang <pair+fwortham4.stevezhere@devbootcamp.com>
2016-09-14 17:37:27 -0500

    fix migration issue

--- mattWiki/db/schema.rb
+++ mattWiki/db/schema.rb
@@ -10,25 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160914210845) do
-
-# ActiveRecord::Schema.define(version: 20160914192838) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "email",           null: false
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       null: false
@@ -43,7 +28,16 @@ ActiveRecord::Schema.define(version: 20160914210845) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
-end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+end
