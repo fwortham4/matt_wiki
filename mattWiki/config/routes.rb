db2dc0e451a757f230683a6d834b8316d752ef8e
Forrest W & Stephen Fang <pair+fwortham4.stevezhere@devbootcamp.com>
2016-09-15 15:39:03 -0500

    Fixes Nav and Able to destroy an Admin account.

--- mattWiki/config/routes.rb
+++ mattWiki/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "homepage#index"
 
   resources :categories
   resources :articles do
@@ -11,5 +10,10 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :admin, only: [:show]
+
+  delete '/admins', to: 'devise/registrations#destroy', as: 'destroyAdmin'
+
+  root "homepage#index"
+
 
 end
