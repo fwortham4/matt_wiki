451059826d0e6191dbb1862b4c1e31b09ca953dd
Forrest W & Stephen Fang <pair+fwortham4.stevezhere@devbootcamp.com>
2016-09-15 09:08:54 -0500

    devise views and set up devise gem

--- mattWiki/config/environments/development.rb
+++ mattWiki/config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  #devise default_url_options
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
