75d4f4ebf3891264d351a6558cc058460b696d80
Forrest W & Stephen Fang <pair+fwortham4.stevezhere@devbootcamp.com>
2016-09-15 14:26:58 -0500

    Adds custom route for Admin and logic for the nav.

--- mattWiki/config/routes.rb
+++ mattWiki/config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :admin, only: [:show]
+  # GET '/admin/show', to: ''
 
 end
