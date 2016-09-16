0bc627de65d33e3cbfa495861da262c7fbcacdb7
Erica Brotherton & Michelle Kelly <pair+ebrothe2.kellymp@devbootcamp.com>
2016-09-15 19:08:26 -0500

    Add back in logic to display author's username and link to profile page

--- mattWiki/app/controllers/articles_controller.rb
+++ mattWiki/app/controllers/articles_controller.rb
@@ -24,21 +24,10 @@ class ArticlesController < ApplicationController
     @version.author_id = current_user.id
     @version.article_id = @article.id
     @version.save
-    # @version = Version.new
-    # @article.update(:version_attributes => {:title => :title})
-    # @article.update(:version_attributes => {:content => :content})
-    # # @version = Version.new(version_params)
-    # @version.article_id = @article.id
-    # @version.save
-
-    # @article.author_id = current_user.id
     @versions = @article.versions
     @version_count = @article.versions.count - 1
     @author = @version.author
     if @article.save
-      # @version = Version.new(version_params)
-      # @version.author_id = current_user.id
-      # @version.article_id = @article.id
       @article
       @version
       @version_count
@@ -52,14 +41,4 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  # private
-  # def version_params
-  #   params.require(:article).permit(:version_attributes[:title, :content])
-  # end
-  # def article_params
-  #   params.require(:article).permit(:category_id)
-  # end
-  # def version_params
-  #   params.require(:version).permit(:title, :content)
-  # end
 end
