a7a1383167e43e3fa400077188c4549f8c529c08
Forrest W & Stephen Fang <pair+fwortham4.stevezhere@devbootcamp.com>
2016-09-16 10:13:42 -0500

    fix seed file and validation error s dfg

--- mattWiki/app/controllers/categories_controller.rb
+++ mattWiki/app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @article_number = @category.articles.length
-    article = @category.articles.first
+    # article = @category.articles.first
   end
 end
