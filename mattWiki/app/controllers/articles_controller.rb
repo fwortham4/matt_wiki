7f3001325ec34b577edfac6d04518f75b21e74be
Erica Brotherton & Michelle Kelly <pair+ebrothe2.kellymp@devbootcamp.com>
2016-09-15 19:03:56 -0500

    HOLY SHIT THAT JUST TOOK 4 HOURS. Create a new article finally works with nested params in nested forms in nested routes

--- mattWiki/app/controllers/articles_controller.rb
+++ mattWiki/app/controllers/articles_controller.rb
@@ -1,24 +1,48 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_admin!
   include SessionHelper
 
   def new
     @article = Article.new
+
   end
 
   def show
     @article = Article.find(params[:id])
   end
 
-
-
   def create
-    # need to add errors
-    @article = Article.new(article_params)
-    @article.author_id = current_user.id
+
+    @article = Article.new(category_id: params["article"]["category_id"])
+    @article.save
+    @version = Version.new
+
+    version_title = params["article"]["version"]["title"]
+    version_content = params["article"]["version"]["content"]
+
+    @version.title = version_title
+    @version.content = version_content
+    @version.author_id = current_user.id
+    @version.article_id = @article.id
+    @version.save
+    # @version = Version.new
+    # @article.update(:version_attributes => {:title => :title})
+    # @article.update(:version_attributes => {:content => :content})
+    # # @version = Version.new(version_params)
+    # @version.article_id = @article.id
+    # @version.save
+
     # @article.author_id = current_user.id
+    @versions = @article.versions
+    @version_count = @article.versions.count - 1
+    @author = @version.author
     if @article.save
-      redirect_to @article
+      # @version = Version.new(version_params)
+      # @version.author_id = current_user.id
+      # @version.article_id = @article.id
+      @article
+      @version
+      @version_count
+      render 'versions/show'
     else
       render 'new'
     end
@@ -28,9 +52,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  private
-  def article_params
-    params.require(:article).permit(:title, :content, :category_id)
-    # maybe category ?
-  end
+  # private
+  # def version_params
+  #   params.require(:article).permit(:version_attributes[:title, :content])
+  # end
+  # def article_params
+  #   params.require(:article).permit(:category_id)
+  # end
+  # def version_params
+  #   params.require(:version).permit(:title, :content)
+  # end
 end
--- mattWiki/app/models/article.rb
+++ mattWiki/app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :category
   has_many :versions
+  accepts_nested_attributes_for :versions
   validates_presence_of :category_id
 
 end
